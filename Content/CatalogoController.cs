19f75d62524947d6799e4ec9f91ed7162cc36f47
JossNB <joselinegarciao09@gmail.com>
2024-12-07 12:06:14 -0600

    Se agrega el proyecto

---
+++ Content/CatalogoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PWAPrueba.Content
+{
+    public class CatalogoController : Controller
+    {
+        // GET: Catalogo
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+﻿const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+    "/",
+    "/css/EstiloCatalogo.css",
+    "/images/Producto1.jpg",
+    "/images/Producto2.jpg",
+    "/images/Producto3.jpg",
+    "/images/Producto5.jpg",
+    "/Content/icons/icon-192x192.png",
+    "/Content/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Archivos en caché");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName)
+                    }
+                })
+            )
+        })
+    )
+})
